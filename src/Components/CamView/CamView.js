4cc640d57ce36bce3978797ec8a86b3a17f57e5f
Kuba <jkb.kam@gmail.com>
2019-07-09 01:37:55 +0200

    date now added, todo- section contact

--- src/Components/CamView/CamView.js
+++ src/Components/CamView/CamView.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import moment from 'moment'
 import './CamView.css';
 
 class CamView extends Component {
 
-
+  state = {
+    dateFormatted: moment().format('DD-MM-YYYY')
+  }
   render() {
+    const NewDate = this.state.dateFormatted;
     return (
       <div className="view">
+        <p>{NewDate}</p>
         <img src={this.props.src} alt="pgs-live-cam" title="pgs-live-cam" />
       </div>
     );
